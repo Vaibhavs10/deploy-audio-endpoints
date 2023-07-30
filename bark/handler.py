ac785d142ec08a07c740f6fd3733eac514091a5a
Vaibhav Srivastav <vaibhavs10@gmail.com>
2023-07-30 15:49:49 +0200

    initial commit - bark

---
+++ bark/handler.py
@@ -0,0 +1,36 @@
+from typing import Dict, List, Any
+from transformers import AutoProcessor, MusicgenForConditionalGeneration
+import torch
+
+class EndpointHandler:
+    def __init__(self, path="facebook/musicgen-large"):
+        # load model and processor from path
+        self.processor = AutoProcessor.from_pretrained(path)
+        self.model = MusicgenForConditionalGeneration.from_pretrained(path, torch_dtype=torch.float16).to("cuda")
+
+    def __call__(self, data: Dict[str, Any]) -> Dict[str, str]:
+        """
+        Args:
+            data (:dict:):
+                The payload with the text prompt and generation parameters.
+        """
+        # process input
+        inputs = data.pop("inputs", data)
+        parameters = data.pop("parameters", None)
+
+        # preprocess
+        inputs = self.processor(
+            text=[inputs],
+            padding=True,
+            return_tensors="pt",).to("cuda")
+
+        # pass inputs with all kwargs in data
+        if parameters is not None:
+            outputs = self.model.generate(**inputs, **parameters)
+        else:
+            outputs = self.model.generate(**inputs,)
+
+        # postprocess the prediction
+        prediction = outputs[0].cpu().numpy()
+
+        return [{"generated_audio": prediction}]
